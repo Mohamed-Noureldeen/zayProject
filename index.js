ce55eb40476fdec93c51ce368e2a10817c9ffd9e
Mohamed <mohamed@2gmail.com>
2024-07-31 14:47:17 +0300

    js file

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("fffff")
+let menu = document.querySelector(".menu");
+let nav = document.querySelector(".nav");
+let opc=0;
+console.log(menu);
+console.log(nav)
+menu.addEventListener("click", function () {
+
+    nav.classList.toggle("visiablal")
+  
+})
